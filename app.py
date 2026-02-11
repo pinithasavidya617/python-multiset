279e0abf6fefedb6d4dc11ae25e41d9ffbee74f4
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-02-11 11:45:41 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+class Multiset:
+
+    def __init__(self):
+        self.data = {}
+
+    def add(self, val):
+        if val in self.data:
+            self.data[val] += 1
+        else:
+            self.data[val] = 1
+
+    def remove(self, val):
+        if val in self.data:
+            self.data[val] -= 1
+            if self.data[val] == 0:
+                del self.data[val]
+
+    def __contains__(self, val):
+        return val in self.data
+
+    def __len__(self):
+        return sum(self.data.values())
